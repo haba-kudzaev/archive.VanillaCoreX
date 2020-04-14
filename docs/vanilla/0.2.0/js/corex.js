7d6c9a7b465897dee5b1caf6542eb5b5b46424cd
rx1310 <rx1310@inbox.ru>
2020-04-14 07:49:55 +0300

    Тест: реализация GitHub Pages

---
+++ docs/vanilla/0.2.0/js/corex.js
@@ -0,0 +1,122 @@
+/*!
+ * @author      rx1310 <rx1310@inbox.ru>
+ * @copyright   Copyright (c) o1310, 2020
+ * @license     MIT License
+ */
+ 
+var CoreX = {
+	ID_BACKDROP: "CoreX_UI_backdrop",
+	ID_THEME_LIGHT: "COREX_UI_THEME_LIGHT",
+	ID_THEME_DARK: "COREX_UI_THEME_DARK"
+};
+ 
+/* =============
+   CoreX JS init */
+function init() {
+
+	initLazyLoadImg();
+	initBackdrop();
+	initNavDrawer();
+	
+}
+
+/* ==================
+   CoreX Theme Engine */
+function setTheme(themeName) {
+	localStorage.setItem("theme", themeName);
+	document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+	if (localStorage.getItem("theme") === CoreX.ID_THEME_DARK) {
+		setTheme(CoreX.ID_THEME_LIGHT);
+	} else {
+		setTheme(CoreX.ID_THEME_DARK);
+	}
+}
+
+function applyTheme() {
+	if (localStorage.getItem("theme") === CoreX.ID_THEME_DARK) {
+		setTheme(CoreX.ID_THEME_DARK);
+	} else {
+		setTheme(CoreX.ID_THEME_LIGHT);
+	}
+} applyTheme();
+
+/* =================
+   CoreX LazyLoadImg */
+function initLazyLoadImg() {
+	[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+		img.setAttribute('src', img.getAttribute('data-src'));
+		img.onload = function() {
+			img.removeAttribute('data-src');
+		};
+	});
+}
+
+/* ==============
+   CoreX Backdrop */
+function initBackdrop() {
+
+	var a = document.createElement("div");
+
+	a.className = "backdrop";
+	a.style.backgroundColor = "rgba(0, 0, 0, 0);";
+	a.style.visibility = "hidden";
+	a.id = CoreX.ID_BACKDROP;
+	a.onclick = function() {
+		hideNavDrawer();
+	};
+
+	document.body.appendChild(a);
+
+}
+
+function showBackdrop() {
+	document.getElementById(CoreX.ID_BACKDROP).style.visibility = "visible";
+	document.getElementById(CoreX.ID_BACKDROP).style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+}
+
+function hideBackdrop() {
+	document.getElementById(CoreX.ID_BACKDROP).style.visibility = "hidden";
+	document.getElementById(CoreX.ID_BACKDROP).style.backgroundColor = "rgba(0, 0, 0, 0)";
+}
+
+/* ===============
+   CoreX NavDrawer */
+function initNavDrawer() {
+
+	var a = document.getElementsByClassName("navdrawer")[0];
+
+	if (a !== undefined) {
+		a.style.left = "-300px";
+	}
+
+}
+
+function showNavDrawer() {
+	
+	showBackdrop();
+
+	var a = document.getElementsByClassName("navdrawer")[0];
+
+	if (a !== undefined) {
+		a.style.left = "0px";
+		a.style.visibility = "visible";
+	}
+
+}
+
+function hideNavDrawer() {
+	
+	hideBackdrop();
+
+	var a = document.getElementsByClassName("navdrawer")[0];
+
+	if (a !== undefined) {
+		a.style.left = "-300px";
+	}
+
+}
+
+window.onload = init;
